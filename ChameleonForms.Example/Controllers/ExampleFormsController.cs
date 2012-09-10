c2ca9d1160a0e964f78f4719c1783cccbec6d42a
Robert Moore <me@robdmoore.id.au>
2012-09-10 23:54:55 +0800

    Added example form

---
+++ ChameleonForms.Example/Controllers/ExampleFormsController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace ChameleonForms.Example.Controllers
+{
+    public class ExampleFormsController : Controller
+    {
+        public ActionResult Form1()
+        {
+            return View();
+        }
+    }
+}
--- ChameleonForms.Example/Forms/ChameleonForm.cs
+++ ChameleonForms.Example/Forms/ChameleonForm.cs
@@ -1,14 +1,21 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
 
 namespace ChameleonForms.Example.Forms
 {
-    public class ChameleonForm<TModel>
+    public class ChameleonForm<TModel> : IDisposable
     {
         private readonly HtmlHelper<TModel> _helper;
 
         public ChameleonForm(HtmlHelper<TModel> helper)
         {
             _helper = helper;
+            // todo
+        }
+
+        public void Dispose()
+        {
+            // todo
         }
     }
 }
