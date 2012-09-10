c1af366a1a5b7d7b7fbfdea4377705393684d928
Robert Moore <me@robdmoore.id.au>
2012-09-10 23:44:54 +0800

    Added homepage for example web project

---
+++ ChameleonForms.Example/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace ChameleonForms.Example.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- ChameleonForms.Example/Global.asax.cs
+++ ChameleonForms.Example/Global.asax.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
-using System.Web.Mvc;
-using System.Web.Routing;
+﻿using System.Web.Routing;
 
 namespace ChameleonForms.Example
 {
@@ -12,9 +6,6 @@ namespace ChameleonForms.Example
     {
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
-
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
     }
