5390c712e635abb5d278f498166f290ec92f13f1
Robert Moore <me@robdmoore.id.au>
2012-09-10 23:50:09 +0800

    Added extension method to start off the process

---
+++ ChameleonForms.Example/Forms/ChameleonForm.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace ChameleonForms.Example.Forms
+{
+    public class ChameleonForm<TModel>
+    {
+        private readonly HtmlHelper<TModel> _helper;
+
+        public ChameleonForm(HtmlHelper<TModel> helper)
+        {
+            _helper = helper;
+        }
+    }
+}
