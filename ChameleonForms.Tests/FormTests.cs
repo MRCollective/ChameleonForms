7ca786d35c893c983298701a0e8467f711122255
Robert Moore <me@robdmoore.id.au>
2012-09-14 22:19:16 +0800

    Added initial unit test for form

---
+++ ChameleonForms.Tests/FormTests.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Net.Http;
+using System.Web.Mvc;
+using ChameleonForms.Tests.Helpers;
+using NUnit.Framework;
+
+namespace ChameleonForms.Tests
+{
+    [TestFixture]
+    class FormShould
+    {
+        [Test]
+        public void Store_html_helper()
+        {
+            var helper = new HtmlHelper<object>(new ViewContext(), new ViewPage());
+            helper.ViewContext.Writer = new StringWriter();
+            var f = new Form<object, DummyFormTemplate>(helper, string.Empty, HttpMethod.Get, string.Empty);
+
+            Assert.That(f.HtmlHelper, Is.EqualTo(helper));
+        }
+    }
+}
---
+++ ChameleonForms.Tests/Helpers/DummyFormTemplate.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Http;
+using System.Web;
+using ChameleonForms.Templates;
+
+namespace ChameleonForms.Tests.Helpers
+{
+    class DummyFormTemplate : IFormTemplate
+    {
+        public IHtmlString BeginForm(string action, HttpMethod method, string enctype)
+        {
+            return new HtmlString(string.Format("BeginForm|{0}|{1}|{2}", action, method.Method.ToLower(), enctype));
+        }
+
+        public IHtmlString EndForm()
+        {
+            return new HtmlString("EndForm");
+        }
+
+        public IHtmlString BeginSection(string title)
+        {
+            return new HtmlString(string.Format("BeginSection|{0}", title));
+        }
+
+        public IHtmlString EndSection()
+        {
+            return new HtmlString("EndSection");
+        }
+
+        public IHtmlString Field(IHtmlString elementHtml, IHtmlString labelHtml, IHtmlString validationHtml)
+        {
+            return new HtmlString(string.Format("Field|{0}|{1}|{2}", elementHtml, labelHtml, validationHtml));
+        }
+    }
+}
