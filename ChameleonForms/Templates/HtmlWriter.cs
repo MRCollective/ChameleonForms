6ffbadb8a05d6c97f64ee4df7f3f33bb44530b84
Robert Moore <me@robdmoore.id.au>
2012-11-01 22:26:12 +0800

    Removing unused method

--- ChameleonForms/Templates/HtmlWriter.cs
+++ ChameleonForms/Templates/HtmlWriter.cs
@@ -8,15 +8,6 @@ namespace ChameleonForms.Templates
 {
     public static class HtmlWriter
     {
-        public static IHtmlString Attribute(string name, string value)
-        {
-            if (value == null)
-                return new HtmlString(string.Empty);
-
-            //Todo: encode the values here
-            return new HtmlString(string.Format(" {0}=\"{1}\"", name, value));
-        }
-
         public static IHtmlString BuildFormTag(string action, FormMethod method, object htmlAttributes = null, EncType? encType = null)
         {
             var tagBuilder = new TagBuilder("form");
