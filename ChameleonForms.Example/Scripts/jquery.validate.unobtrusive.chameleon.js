b01ee4e35208f9278312394f8a9039345a5f0f38
Robert Moore <me@robdmoore.id.au>
2013-06-25 23:37:07 +0800

    Added more unit tests and some more implementation for the client-side date validation

--- ChameleonForms.Example/Scripts/jquery.validate.unobtrusive.chameleon.js
+++ ChameleonForms.Example/Scripts/jquery.validate.unobtrusive.chameleon.js
@@ -1,48 +1,103 @@
 ﻿jQuery.validator.methods.date = function (value, element) {
-    var $ = jQuery;
-
     if (this.optional(element))
         return true;
 
-    var format = $(element).data("val-format");
+    var format = jQuery(element).data("val-format");
     if (format == "")
         return !/Invalid|NaN/.test(new Date(value).toString());
 
-    var dateFormat = format.split(" ")[0];
-    var timeFormat = format.split(" ")[1];
+    var dateFormat = format;
+    // todo: support time too
 
+    var getDateParser = function (regex, dayIndex, monthIndex, yearIndex) {
+        return {
+            regex: regex,
+            dayIndex: dayIndex,
+            monthIndex: monthIndex,
+            yearIndex: yearIndex
+        };
+    };
+
+    var dateParser;
     switch (dateFormat) {
         case "d/M/yyyy":
+            dateParser = getDateParser(/^(\d{1,2})\/(\d{1,2})\/(\d{4})$/, 1, 2, 3);
+            break;
         case "d-M-yyyy":
+            dateParser = getDateParser(/^(\d{1,2})-(\d{1,2})-(\d{4})$/, 1, 2, 3);
             break;
         case "d/M/yy":
+            dateParser = getDateParser(/^(\d{1,2})\/(\d{1,2})\/(\d{2})$/, 1, 2, 3);
+            break;
         case "d-M-yy":
+            dateParser = getDateParser(/^(\d{1,2})-(\d{1,2})-(\d{2})$/, 1, 2, 3);
             break;
         case "dd/MM/yyyy":
+            dateParser = getDateParser(/^(\d{2})\/(\d{2})\/(\d{4})$/, 1, 2, 3);
+            break;
         case "dd-MM-yyyy":
+            dateParser = getDateParser(/^(\d{2})-(\d{2})-(\d{4})$/, 1, 2, 3);
             break;
         case "dd/MM/yy":
+            dateParser = getDateParser(/^(\d{2})\/(\d{2})\/(\d{2})$/, 1, 2, 3);
+            break;
         case "dd-MM-yy":
+            dateParser = getDateParser(/^(\d{2})-(\d{2})-(\d{2})$/, 1, 2, 3);
             break;
         case "M/d/yyyy":
+            dateParser = getDateParser(/^(\d{1,2})\/(\d{1,2})\/(\d{4})$/, 2, 1, 3);
+            break;
         case "M-d-yyyy":
+            dateParser = getDateParser(/^(\d{1,2})-(\d{1,2})-(\d{4})$/, 2, 1, 3);
             break;
         case "M/d/yy":
+            dateParser = getDateParser(/^(\d{1,2})\/(\d{1,2})\/(\d{2})$/, 2, 1, 3);
+            break;
         case "M-d-yy":
+            dateParser = getDateParser(/^(\d{1,2})-(\d{1,2})-(\d{2})$/, 2, 1, 3);
             break;
         case "MM/dd/yyyy":
-        case "MM-dd-yyyy":
+            dateParser = getDateParser(/^(\d{2})\/(\d{2})\/(\d{4})$/, 2, 1, 3);
             break;
-        case "MM/dd/yy":
+        case "MM-dd-yyyy":
+            dateParser = getDateParser(/^(\d{2})-(\d{2})-(\d{4})$/, 2, 1, 3);
+            break;
+        case "MM/ddyy":
+            dateParser = getDateParser(/^(\d{2})\/(\d{2})\/(\d{2})$/, 2, 1, 3);
+            break;
         case "MM-dd-yy":
+            dateParser = getDateParser(/^(\d{2})-(\d{2})-(\d{2})$/, 2, 1, 3);
             break;
         case "yyyy/MM/dd":
+            dateParser = getDateParser(/^(\d{4})\/(\d{2})\/(\d{2})$/, 3, 2, 1);
+            break;
         case "yyyy-MM-dd":
+            dateParser = getDateParser(/^(\d{4})-(\d{2})-(\d{2})$/, 3, 2, 1);
             break;
         case "yyyy/M/d":
+            dateParser = getDateParser(/^(\d{4})\/(\d{1,2})\/(\d{1,2})$/, 3, 2, 1);
+            break;
         case "yyyy-M-d":
+            dateParser = getDateParser(/^(\d{4})-(\d{1,2})-(\d{1,2})$/, 3, 2, 1);
             break;
         default:
             return true;
     }
+
+    var match = value.match(dateParser.regex);
+    if (match == null)
+        return false;
+
+    var year = match[dateParser.yearIndex]*1;
+    if (year < 50)
+        year += 2000;
+    if (year < 100)
+        year += 1900;
+
+    var date = new Date(year, match[dateParser.monthIndex]*1-1, match[dateParser.dayIndex]*1);
+
+    if (!/Invalid|NaN/.test(date.toString()))
+        return false;
+
+    return true;
 }
--- ChameleonForms.Tests/Scripts/spec/DateTimeValidationSpec.js
+++ ChameleonForms.Tests/Scripts/spec/DateTimeValidationSpec.js
@@ -1,5 +1,8 @@
 // Jasmine
 /// <reference path="../lib/jasmine-1.3.1/jasmine.js"/>
+/// <reference path="../lib/jasmine.using.js"/>
+// Fixture data
+/// <reference path="../fixtures/DateTimeValidationFixtures.js"/>
 // Libs
 /// <reference path="../src/jquery-1.4.4.js"/>
 /// <reference path="../src/jquery.validate.js"/>
@@ -48,6 +51,30 @@ describe("When validating a date field", function () {
 
             it("should return true if the date is a JavaScript parsable date string", function() {
                 expect(invoke("12/13/2000")).toBe(true);
+            });
+            
+            it("should return false if the date is a JavaScript parsable date string for an invalid date", function () {
+                expect(invoke("13/12/2000")).toBe(false);
+            });
+            
+            it("should return false if the date is not a JavaScript parsable date string for an invalid date", function () {
+                expect(invoke("notadate")).toBe(false);
+            });
+        });
+        
+        describe("input with date-only format", function () {
+            using("valid format and value", ObjectMother.DateTimeFormats.Valid, function (format, value) {
+                it("should return true", function () {
+                    formatString = format;
+                    expect(invoke(value)).toBe(true);
+                });
+            });
+            
+            using("invalid format and value", ObjectMother.DateTimeFormats.Invalid, function (format, value) {
+                it("should return false", function () {
+                    formatString = format;
+                    expect(invoke(value)).toBe(false);
+                });
             });
         });
     });
