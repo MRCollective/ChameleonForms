70cd941423d2c9c79bc1fcab506a36ef81581763
Robert Moore <me@robdmoore.id.au>
2013-06-25 20:05:52 +0800

    [WIP] Adding initial client-side date format validation code and tests

---
+++ ChameleonForms.Example/Scripts/jquery.validate.unobtrusive.chameleon.js
@@ -0,0 +1,48 @@
+﻿jQuery.validator.methods.date = function (value, element) {
+    var $ = jQuery;
+
+    if (this.optional(element))
+        return true;
+
+    var format = $(element).data("val-format");
+    if (format == "")
+        return !/Invalid|NaN/.test(new Date(value).toString());
+
+    var dateFormat = format.split(" ")[0];
+    var timeFormat = format.split(" ")[1];
+
+    switch (dateFormat) {
+        case "d/M/yyyy":
+        case "d-M-yyyy":
+            break;
+        case "d/M/yy":
+        case "d-M-yy":
+            break;
+        case "dd/MM/yyyy":
+        case "dd-MM-yyyy":
+            break;
+        case "dd/MM/yy":
+        case "dd-MM-yy":
+            break;
+        case "M/d/yyyy":
+        case "M-d-yyyy":
+            break;
+        case "M/d/yy":
+        case "M-d-yy":
+            break;
+        case "MM/dd/yyyy":
+        case "MM-dd-yyyy":
+            break;
+        case "MM/dd/yy":
+        case "MM-dd-yy":
+            break;
+        case "yyyy/MM/dd":
+        case "yyyy-MM-dd":
+            break;
+        case "yyyy/M/d":
+        case "yyyy-M-d":
+            break;
+        default:
+            return true;
+    }
+}
---
+++ ChameleonForms.Tests/Scripts/spec/DateTimeValidationSpec.js
@@ -0,0 +1,54 @@
+// Jasmine
+/// <reference path="../lib/jasmine-1.3.1/jasmine.js"/>
+// Libs
+/// <reference path="../src/jquery-1.4.4.js"/>
+/// <reference path="../src/jquery.validate.js"/>
+/// <reference path="../src/jquery.validate.unobtrusive.js"/>
+// Src
+/// <reference path="../src/jquery.validate.unobtrusive.chameleon.js"/>
+
+describe("When validating a date field", function () {
+
+    var element = {};
+    var validatorContext = { optional: function () { return true; }};
+
+    var invoke = function (value) {
+        return jQuery.validator.methods.date.call(validatorContext, value, element);
+    };
+
+    describe("input with no value", function () {
+        
+        beforeEach(function() {
+            spyOn(validatorContext, "optional").andReturn(true);
+        });
+        
+        it("should return true", function () {
+            expect(invoke()).toBe(true);
+        });
+    });
+
+    describe("input with value", function () {
+
+        var formatString;
+
+        beforeEach(function () {
+            spyOn(validatorContext, "optional").andReturn(false);
+            spyOn($.fn, "data").andCallFake(function (key) {
+                if (key == "val-format")
+                    return formatString;
+                return null;
+            });
+        });
+
+        describe("input with no format", function () {
+
+            beforeEach(function() {
+                formatString = "";
+            });
+
+            it("should return true if the date is a JavaScript parsable date string", function() {
+                expect(invoke("12/13/2000")).toBe(true);
+            });
+        });
+    });
+});
