3bdb8f77950ab530b429d3c425a9d3bd89f2bf7e
Matt Davies <matt@mdavies.net>
2013-08-31 20:15:14 +0800

    Server side ExistsIn attribute validation

---
+++ ChameleonForms.Tests/Attributes/ExistsInAttributeTests.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using ChameleonForms.Attributes;
+using ChameleonForms.Example.Controllers;
+using NUnit.Framework;
+
+namespace ChameleonForms.Tests.Attributes
+{
+    [TestFixture]
+    public class ExistsInAttributeShould
+    {
+        [TestCase("", "Test")]
+        [TestCase(null, "Test")]
+        [TestCase("Test", "")]
+        [TestCase("Test", null)]
+        [TestCase("", "")]
+        [TestCase(null, null)]
+        public void Throw_exception_if_existsin_was_initialised_with_null_name_or_value_properties(string valueProperty, string nameProperty)
+        {
+            const string listProperty = "List";
+            var vm = new ViewModelExample();
+            var validationContext = new ValidationContext(vm, null, null);
+            var attribute = new ExistsInAttribute(listProperty, valueProperty, nameProperty);
+
+            Assert.Throws<ArgumentException>(
+                () => attribute.GetValidationResult(vm.ListId, validationContext),
+                "You must pass valid properties for Name and Value to ExistsIn"
+            );
+        }
+
+        [Test]
+        public void Throw_exception_if_list_to_validate_against_does_not_exist()
+        {
+            const string valueProperty = "Id";
+            const string nameProperty = "Name";
+            const string listProperty = "List2";
+            var vm = new ViewModelExample();
+            var validationContext = new ValidationContext(vm, null, null);
+            var attribute = new ExistsInAttribute(listProperty, valueProperty, nameProperty);
+
+            Assert.Throws<Exception>(
+                () => attribute.GetValidationResult(vm.ListId, validationContext),
+                string.Format("No property Model.{0} exists for ExistsIn validation.", valueProperty)
+            );
+        }
+
+        [Test]
+        public void Throw_exception_if_property_to_validate_against_is_not_an_ienumerable()
+        {
+            const string valueProperty = "Id";
+            const string nameProperty = "Name";
+            const string listProperty = "Decimal";
+            var vm = new ViewModelExample();
+            var validationContext = new ValidationContext(vm, null, null);
+            var attribute = new ExistsInAttribute(listProperty, valueProperty, nameProperty);
+
+            Assert.Throws<Exception>(
+                () => attribute.GetValidationResult(vm.ListId, validationContext),
+                string.Format("No property Model.{0} exists for ExistsIn validation.", valueProperty)
+            );
+        }
+
+        [Test]
+        public void Throw_exception_if_list_to_validate_against_is_null()
+        {
+            const string valueProperty = "Id";
+            const string nameProperty = "Name";
+            const string listProperty = "List";
+            var vm = new ViewModelExample
+            {
+                List = null
+            };
+            var validationContext = new ValidationContext(vm, null, null);
+            var attribute = new ExistsInAttribute(listProperty, valueProperty, nameProperty);
+
+            Assert.Throws<Exception>(
+                () => attribute.GetValidationResult(vm.ListId, validationContext),
+                string.Format("Model.{0} is null. Unable to make list for Model.{1}", valueProperty, validationContext.MemberName)
+            );
+        }
+
+        [TestCase(-1)]
+        [TestCase(0)]
+        [TestCase(3)]
+        public void Fail_validation_if_property_value_not_in_list(int testValue)
+        {
+            const string valueProperty = "Id";
+            const string nameProperty = "Name";
+            const string listProperty = "List";
+            var vm = new ViewModelExample { ListId = testValue };
+            var validationContext = new ValidationContext(vm, null, null) { DisplayName = "List id" };
+            var attribute = new ExistsInAttribute(listProperty, valueProperty, nameProperty);
+
+            var result = attribute.GetValidationResult(vm.ListId, validationContext);
+
+            var expectedError = string.Format("The {{0}} field was {0}, but must be one of A, B", testValue);
+            Assert.That(result.ErrorMessage, Is.EqualTo(string.Format(expectedError, validationContext.DisplayName)));
+        }
+
+        [Test]
+        public void Successfully_validate()
+        {
+            const string valueProperty = "Id";
+            const string nameProperty = "Name";
+            const string listProperty = "List";
+            var vm = new ViewModelExample { ListId = 1 };
+            var validationContext = new ValidationContext(vm, null, null);
+            var attribute = new ExistsInAttribute(listProperty, valueProperty, nameProperty);
+
+            var result = attribute.GetValidationResult(vm.ListId, validationContext);
+
+            Assert.That(result, Is.Null, string.Format("Validation failed with message: {0}", attribute.ErrorMessage));
+        }
+    }
+}
--- ChameleonForms/Attributes/ExistsInAttribute.cs
+++ ChameleonForms/Attributes/ExistsInAttribute.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace ChameleonForms.Attributes
@@ -7,7 +11,7 @@ namespace ChameleonForms.Attributes
     /// Indicates that the attributed property value should exist within the list property referenced by the attribute.
     /// </summary>
     [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
-    public class ExistsInAttribute : Attribute, IMetadataAware
+    public class ExistsInAttribute : ValidationAttribute, IMetadataAware
     {
         /// <summary>
         /// Additional Values metadata key for whether this attribute has been applied to the property.
@@ -49,6 +53,56 @@ namespace ChameleonForms.Attributes
             metadata.AdditionalValues[PropertyKey] = _listProperty;
             metadata.AdditionalValues[ValueKey] = _valueProperty;
             metadata.AdditionalValues[NameKey] = _nameProperty;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext context)
+        {
+            if (value == null)
+            {
+                return ValidationResult.Success;
+            }
+            var collection = GetCollectionIfValid(context);
+            var possibleValues = collection.Select(item => item.GetType().GetProperty(_valueProperty).GetValue(item, null).ToString());
+            if (value is IEnumerable)
+            {
+                if ((value as IEnumerable).Cast<object>().All(v => possibleValues.Any(item => item == v.ToString())))
+                {
+                    return ValidationResult.Success;
+                }
+            }
+            else if (possibleValues.Any(item => item == value.ToString()))
+            {
+                return ValidationResult.Success;
+            }
+
+            var choices = string.Join(", ", collection.Select(o => o.GetType().GetProperty(_nameProperty).GetValue(o, null)));
+            ErrorMessage = string.Format("The {0} field was {1}, but must be one of {2}", "{0}", value, choices);
+            return new ValidationResult(FormatErrorMessage(context.DisplayName ?? context.MemberName), new List<string> { _listProperty });
+        }
+
+        private IList<object> GetCollectionIfValid(ValidationContext context)
+        {
+            if (string.IsNullOrEmpty(_nameProperty) || string.IsNullOrEmpty(_valueProperty))
+            {
+                throw new ArgumentException("You must pass valid properties for Name and Value to ExistsIn.");
+            }
+            var collectionProperty = context.ObjectInstance.GetType().GetProperty(_listProperty);
+            if (collectionProperty == null)
+            {
+                throw new Exception(string.Format("No property Model.{0} exists for ExistsIn validation.", _listProperty));
+            }
+            var collectionValue = collectionProperty.GetValue(context.ObjectInstance, null);
+            if (collectionValue == null)
+            {
+                throw new Exception(string.Format("Model.{0} is null. Unable to make list for Model.{1}", _listProperty, context.MemberName));
+            }
+            var collection = collectionValue as IEnumerable;
+            if (collection == null)
+            {
+                throw new Exception(string.Format("Model.{0} is not an IEnumerable. ExistsIn cannot be used to validate against this property.", _listProperty));
+            }
+
+            return collection.Cast<object>().ToList();
         }
     }
 }
