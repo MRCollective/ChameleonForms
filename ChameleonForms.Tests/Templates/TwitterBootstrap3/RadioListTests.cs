927ae946394b0b71ed85e9ee28b9d860d41b7438
Robert Moore <me@robdmoore.id.au>
2013-11-01 00:09:11 +0800

    Added tests to cover radio and checkbox list html for the twitter bootstrap template

---
+++ ChameleonForms.Tests/Templates/TwitterBootstrap3/RadioListTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Web;
+using ApprovalTests.Html;
+using ApprovalTests.Reporters;
+using ChameleonForms.Templates.TwitterBootstrap3;
+using NUnit.Framework;
+
+namespace ChameleonForms.Tests.Templates.TwitterBootstrap3
+{
+    [UseReporter(typeof(DiffReporter))]
+    class RadioListTests_TwitterBootstrapTemplateShould
+    {
+        [Test]
+        public void Render_radio_list()
+        {
+            var t = new TwitterBootstrapFormTemplate();
+
+            var result = t.RadioOrCheckboxList(new IHtmlString[] {new HtmlString("1"), new HtmlString("2")}, false);
+
+            HtmlApprovals.VerifyHtml(result.ToHtmlString());
+        }
+
+        [Test]
+        public void Render_checkbox_list()
+        {
+            var t = new TwitterBootstrapFormTemplate();
+
+            var result = t.RadioOrCheckboxList(new IHtmlString[] { new HtmlString("1"), new HtmlString("2") }, true);
+
+            HtmlApprovals.VerifyHtml(result.ToHtmlString());
+        }
+    }
+}
