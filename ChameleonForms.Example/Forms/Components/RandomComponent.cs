b5ae2ee9b5362dc34683c00d5291e08f6d6cf083
zabulus <zabulus12@gmail.com>
2015-01-03 22:51:16 +0200

    Remove IFormTemplate

--- ChameleonForms.Example/Forms/Components/RandomComponent.cs
+++ ChameleonForms.Example/Forms/Components/RandomComponent.cs
@@ -9,52 +9,52 @@ namespace ChameleonForms.Example.Forms.Components
     /// <summary>
     /// Illustrates using a custom template class and wrapping an unencoded string output.
     /// </summary>
-    public class RandomComponent<TModel> : IFormComponent<TModel, RandomFormTemplate>
+    public class RandomComponent<TModel> : IFormComponent<TModel>
     {
-        public IForm<TModel, RandomFormTemplate> Form { get; private set; }
+        public IForm<TModel> Form { get; private set; }
 
-        public RandomComponent(IForm<TModel, RandomFormTemplate> form)
+        public RandomComponent(IForm<TModel> form)
         {
             Form = form;
         }
 
         public override string ToString()
         {
-            return Form.Template.RandomComponent();
+            return ((RandomFormTemplate)Form.Template).RandomComponent();
         }
     }
 
     /// <summary>
     /// Illustrates using a custom template class and wrapping an encoded HTML string output.
     /// </summary>
-    public class RandomComponent2<TModel> : IFormComponent<TModel, RandomFormTemplate>, IHtmlString
+    public class RandomComponent2<TModel> : IFormComponent<TModel>, IHtmlString
     {
-        public IForm<TModel, RandomFormTemplate> Form { get; private set; }
+        public IForm<TModel> Form { get; private set; }
 
-        public RandomComponent2(IForm<TModel, RandomFormTemplate> form)
+        public RandomComponent2(IForm<TModel> form)
         {
             Form = form;
         }
 
         public string ToHtmlString()
         {
-            return Form.Template.RandomComponent2().ToHtmlString();
+            return ((RandomFormTemplate)Form.Template).RandomComponent2().ToHtmlString();
         }
     }
 
     public static class RandomComponentExtensions
     {
-        public static Form<TModel, RandomFormTemplate> BeginRandomForm<TModel>(this HtmlHelper<TModel> helper, string action, FormMethod method, object htmlAttributes = null, EncType? enctype = null)
+        public static Form<TModel> BeginRandomForm<TModel>(this HtmlHelper<TModel> helper, string action, FormMethod method, object htmlAttributes = null, EncType? enctype = null)
         {
-            return new Form<TModel, RandomFormTemplate>(helper, new RandomFormTemplate(), action, method, htmlAttributes.ToHtmlAttributes(), enctype);
+            return new Form<TModel>(helper, new RandomFormTemplate(), action, method, htmlAttributes.ToHtmlAttributes(), enctype);
         }
 
-        public static RandomComponent<TModel> RandomComponent<TModel>(this Form<TModel, RandomFormTemplate>  form)
+        public static RandomComponent<TModel> RandomComponent<TModel>(this Form<TModel>  form)
         {
             return new RandomComponent<TModel>(form);
         }
 
-        public static RandomComponent2<TModel> RandomComponent2<TModel>(this Form<TModel, RandomFormTemplate> form)
+        public static RandomComponent2<TModel> RandomComponent2<TModel>(this Form<TModel> form)
         {
             return new RandomComponent2<TModel>(form);
         }
