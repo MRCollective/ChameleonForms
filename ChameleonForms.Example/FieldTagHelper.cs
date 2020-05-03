479049e4cfdae14302b1ab68f826682b49e2e7f8
Rob Moore <me@robdmoore.id.au>
2020-05-03 12:57:11 +0800

    Added nested fields and the beginning of field configuration

--- ChameleonForms.Example/FieldTagHelper.cs
+++ ChameleonForms.Example/FieldTagHelper.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections.Generic;
 using System.Linq.Expressions;
 using ChameleonForms.Component;
+using ChameleonForms.Component.Config;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
@@ -8,8 +10,35 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace ChameleonForms.Example
 {
+    public interface IFieldConfigurableTagHelper
+    {
+        public string AddClass { get; set; }
+        public string Label { get; set; }
+
+        public IDictionary<string, string> Attrs { get; set; }
+    }
+
+    public static class FieldConfigurableTagHelperExtensions
+    {
+        public static IFieldConfiguration Configure(this IFieldConfiguration fc, IFieldConfigurableTagHelper th)
+        {
+            if (th.Label != null)
+                fc.Label(th.Label);
+
+            if (th.AddClass != null)
+                fc.AddClass(th.AddClass);
+
+            foreach (var attrKey in th.Attrs.Keys)
+            {
+                fc.Attr(attrKey, th.Attrs[attrKey]);
+            }
+
+            return fc;
+        }
+    }
+
     //[HtmlTargetElement(TagStructure = TagStructure.WithoutEndTag)]
-    public class FieldTagHelper : TagHelper
+    public class FieldTagHelper : TagHelper, IFieldConfigurableTagHelper
     {
         [HtmlAttributeNotBound]
         [ViewContext]
@@ -17,6 +46,7 @@ namespace ChameleonForms.Example
 
 
         public ModelExpression For { get; set; }
+        public IFieldConfiguration Configuration { get; set; }
         
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
@@ -29,9 +59,31 @@ namespace ChameleonForms.Example
             var helper = ViewContext.HttpContext.RequestServices.GetRequiredService<IHtmlHelper<TModel>>();
             (helper as HtmlHelper<TModel>)?.Contextualize(ViewContext);
             var s = ViewContext.ViewData["ChameleonFormSection"] as Section<TModel>;
+            var ff = ViewContext.ViewData["ChameleonFormField"] as Field<TModel>;
 
-            ViewContext.Writer.WriteLine(s.FieldFor(@for));
-            output.SuppressOutput();
+            if (s != null)
+            {
+                if (output.TagMode == TagMode.SelfClosing)
+                {
+                    output.TagMode = TagMode.StartTagAndEndTag;
+                    output.TagName = null;
+                    output.Content.SetHtmlContent(s.FieldFor(@for).Configure(this));
+                }
+                else
+                {
+                    using (var field = s.BeginFieldFor(@for, Field.Configure().Configure(this)))
+                    {
+                        ViewContext.ViewData["ChameleonFormField"] = field;
+                        ViewContext.Writer.WriteLine(output.GetChildContentAsync().GetAwaiter().GetResult());
+                    }
+                }
+            }
+            else
+            {
+                output.TagMode = TagMode.StartTagAndEndTag;
+                output.TagName = null;
+                output.Content.SetHtmlContent(ff.FieldFor(@for).Configure(this));
+            }
         }
 
         static Expression GetPropertySelector(Type modelType, string propertyName)
@@ -41,5 +93,10 @@ namespace ChameleonForms.Example
             var exp = Expression.Lambda(property, arg);
             return exp;
         }
+
+        public string AddClass { get; set; }
+        public string Label { get; set; }
+        [HtmlAttributeName("attrs", DictionaryAttributePrefix = "attr-")]
+        public IDictionary<string, string> Attrs { get; set; } = new Dictionary<string, string>();
     }
 }
